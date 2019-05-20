df93b17a6a8e8a7d4b745ef8bf5a9eb62a740132
SnowNooDLe <sdw123123@gmail.com>
2019-05-21 00:30:07 +1200

    working Vue version
    But need to check whether output is correct

--- vue/src/Controller.js
+++ vue/src/Controller.js
@@ -2,11 +2,12 @@
 /* eslint-disable no-undef */
 /* jshint esversion: 6 */
 Vue.component('C1', {
-  template: '<div><input type="file" id="myFile" @change="addFiles" multiple><br><button v-if="files.length >= 2" @click="submitFiles">Submit Files</button></div>',
+  template: '<div><input type="file" id="myFile" @change="addFiles" multiple><br><button v-if="files.length == 2" @click="submitFiles">Submit Files</button><br><button @click="calcCorrelation">Show Correlation</button> <br> {{ correlation }}</div>',
   data: function() {
     return{
       files: [],
-      output: ""
+      numbers: [],
+      correlation: "What Am I"
     }
   },
   methods:{
@@ -16,12 +17,39 @@ Vue.component('C1', {
       }
     },
     submitFiles: function(event){
-      const file = this.files[0];
-      const reader = new FileReader();
+      for (let file of this.files){
+        const reader = new FileReader();
 
-      reader.onload = e => console.log(e.target.result);
+        reader.onload = e => {
+          let ct = e.target.result.split('\r\n').map(x => parseFloat(x));
+          console.log(ct);
+          this.numbers.push(ct);
+        }
+        reader.readAsText(file);
+      }
+    },
+    calcCorrelation: function(){
+      let sumOfX = 0;
+      let sumOfX2 = 0;
+      let sumOfY = 0;
+      let sumOfY2 = 0;
+      let xTimesY = 0;
+      let n = this.numbers[0].length;
 
-      reader.readAsText(file);
+      for (i = 0; i < n; i++){
+        sumOfX += this.numbers[0][i];
+        sumOfY += this.numbers[1][i];
+        sumOfX2 += Math.pow(this.numbers[0][i], 2);
+        sumOfY2 += Math.pow(this.numbers[1][i], 2);
+        xTimesY += this.numbers[0][i] * this.numbers[1][i];
+      }
+      
+      let numerator = (n * xTimesY) - (sumOfX * sumOfY);
+      let left = n * sumOfX2 - Math.pow(sumOfX, 2);
+      let right = n * sumOfY2 - Math.pow(sumOfY, 2);
+      let denominator = Math.sqrt(left * right);
+
+      this.correlation = numerator / denominator;
     }
   }
 })
