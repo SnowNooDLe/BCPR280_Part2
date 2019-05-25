53be6c9309e092c81c8f1d07029f30c2f1e883cf
SnowNooDLe <sdw123123@gmail.com>
2019-05-26 00:08:12 +1200

    warning popup
    warning popup if files arent submitted or two files havent been chosen

--- src/ControllerVue.js
+++ src/ControllerVue.js
@@ -2,12 +2,12 @@
 /* eslint-disable no-undef */
 /* jshint esversion: 6 */
 Vue.component('c1', {
-  template: '<div><input type="file" @change="addFile"><br><input type="file" @change="addFile"><br><br><button v-if="files.length == 2" @click="submitFiles" class="btn btn-primary">Submit Files</button><br><br><button v-if = "status == true" @click="calcCorrelation" class="btn btn-primary">Show Correlation</button> <br> {{ correlation }}<br><button v-if = "status == true" @click="calcRegression" class="btn btn-primary">Show Regression</button> <br> <label>Regression Beta One </label> {{ regressionBetaOne }} <br> <label>Regression Beta Zero </label> {{ regressionBetaZero }}</div>',
+  template: '<div><input type="file" @change="addFile"><br><input type="file" @change="addFile"><br><br><button @click="submitFiles" class="btn btn-primary">Submit Files</button><br><br><button @click="calcCorrelation" class="btn btn-primary">Show Correlation</button> <br> {{ correlation }}<br><button @click="calcRegression" class="btn btn-primary">Show Regression</button> <br> <label>Regression Beta One </label> {{ regressionBetaOne }} <br> <label>Regression Beta Zero </label> {{ regressionBetaZero }}</div>',
   data: function() {
     return{
       files: [],
       numbers: [],
-      status: false,
+      submitStatus: false,
       correlation : "I am correlation",
       regressionBetaOne : "I am regression beta one.",
       regressionBetaZero : "I am regression beta zero."
@@ -20,31 +20,39 @@ Vue.component('c1', {
       
     },
     submitFiles: function(event){
-      for (let file of this.files){
-        const reader = new FileReader();
-
-        reader.onload = e => {
-          let ct = e.target.result.split('\r\n').map(x => parseFloat(x));
-          console.log(ct);
-          this.numbers.push(ct);
+      if (this.files.length != 2){
+        alert("You need to submit two files then click it again.");
+      } else {
+        for (let file of this.files){
+          const reader = new FileReader();
+          reader.onload = e => {
+            let ct = e.target.result.split('\r\n').map(x => parseFloat(x));
+            this.numbers.push(ct);
+          }
+          reader.readAsText(file);
         }
-        reader.readAsText(file);
-        this.status = true;
+        this.submitStatus = true;
       }
     },
     calcCorrelation: function(){
-      aCalculator.calculateRequirements(this.numbers);
-      aCalculator.calcCorrelation();
-      this.correlation = aCalculator.correlation;
-      
+      if (this.submitStatus != true){
+        alert("Please submit two files first.");
+      } else {
+        aCalculator.calculateRequirements(this.numbers);
+        aCalculator.calcCorrelation();
+        this.correlation = aCalculator.correlation;
+      }
     },
     calcRegression: function(){
-      aCalculator.clacRegressionBetaOne();
+      if (this.submitStatus != true){
+        alert("Please submit two files first.");
+      } else {
+        aCalculator.clacRegressionBetaOne();
       aCalculator.clacRegressionBetaZero();
       this.regressionBetaOne = aCalculator.regressionBetaOne;
       this.regressionBetaZero = aCalculator.regressionBetaZero;
+      }
     }
-
   }
 })
 
